962b8e5794289ce40d6d5842fdf2b4729f0f0f06
RaphaeleL <raphaele.salvatore@outlook.de>
2021-04-13 13:00:45 +0200

    New Folder Structure

---
+++ T_20210319/T_20210319_empty.py
@@ -0,0 +1,39 @@
+class Termin_1: 
+
+# PLEASE REPLACE "PASS" WITH YOUR CODE. THE SOLUTION SHOULD BE RETURNED!!!
+
+    def aufgabe_4a(self): 
+        pass
+
+    def aufgabe_4b(self): 
+        pass
+
+    def aufgabe_5(self):
+        pass
+    
+    def aufgabe_6(self):
+        pass
+
+    def aufgabe_7(self):
+        pass
+
+    def aufgabe_8(self): 
+        pass
+    
+    def aufgabe_9(self): 
+        pass
+
+    def aufgabe_10(self): 
+        pass
+    
+    def aufgabe_11(self): 
+        pass
+
+    def aufgabe_12(self): 
+        pass
+
+    def aufgabe_13(self):
+        pass
+
+# PLEASE REPLACE THE THE CALLING METHOD WITH THAT ONE YOU WANT TO TEST!!!
+print(Termin_1().aufgabe_10())
---
+++ T_20210326/T_20210326_empty.py
@@ -0,0 +1,31 @@
+class Termin_2: 
+
+# PLEASE REPLACE "PASS" WITH YOUR CODE. THE SOLUTION SHOULD BE RETURNED!!!
+
+    def aufgabe_1(self): 
+        pass
+
+    def aufgabe_2(self): 
+        pass
+        
+    def aufgabe_3(self): 
+        pass
+
+    def aufgabe_4(self):
+        pass
+
+    def aufgabe_5(self): 
+        pass
+
+    def aufgabe_6(self): 
+        pass
+    
+    def aufgabe_7(self):
+        pass
+
+    def aufgabe_8(self):
+        pass
+
+# PLEASE REPLACE THE THE CALLING METHOD WITH THAT ONE YOU WANT TO TEST!!!
+
+print(Termin_2().aufgabe_8())
